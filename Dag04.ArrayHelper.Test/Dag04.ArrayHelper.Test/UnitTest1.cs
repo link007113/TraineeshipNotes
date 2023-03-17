a9e5c9278761708bdd1b24a5f3ba3b447c62483d
Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com <Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com>
2023-03-17 14:49:58 +0100

    Multiple Unit Tests

--- Dag04.ArrayHelper.Test/Dag04.ArrayHelper.Test/UnitTest1.cs
+++ Dag04.ArrayHelper.Test/Dag04.ArrayHelper.Test/UnitTest1.cs
@@ -4,17 +4,37 @@ namespace Dag04.ArrayHelper.Test
     public class ArrayHelperTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SumOfZero()
         {
+            // Arrange
+            int[] array = new int[0];
+
+            // Act
+            ArrayHelper arrayHelper = new ArrayHelper();
+
+            int som = arrayHelper.SomVanElementen(array);
+
+
+            // Assert
+            Assert.AreEqual(0, som);
+        }
+        [TestMethod]
+        public void SumOfOneAndTwo()
+        {
+            // Arrange
             int[] array = new int[2]
                 {
                     1,
                     2
                 };
+
+            // Act
             ArrayHelper arrayHelper = new ArrayHelper();
 
             int som = arrayHelper.SomVanElementen(array);
 
+
+            // Assert
             Assert.AreEqual(3, som);
         }
     }
