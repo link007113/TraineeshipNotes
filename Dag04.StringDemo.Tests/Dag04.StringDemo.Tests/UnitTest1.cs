9cd8a322fe3a2a3dcf5b0c3c47bfc1dbf4aa663f
Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com <Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com>
2023-03-17 15:37:42 +0100

    Made first version of String alteration tests

---
+++ Dag04.StringDemo.Tests/Dag04.StringDemo.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+using Dag04.StringDemo;
+
+namespace Dag04.StringDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAToE()
+        {
+            string input = "water";
+            string output = StringOperations.AToE(input);
+            Assert.AreEqual("weter", output);
+        }
+
+        [TestMethod]
+        public void TestAToEAndEToI()
+        {
+            string input = "water";
+            string output = StringOperations.AToEAndEToI(input);
+            Assert.AreEqual("wetir", output);
+        }
+
+        [TestMethod]
+        public void TestIsPalindroom()
+        {
+            string input1 = "water";
+            bool output1 = StringOperations.IsPalindrome(input1);
+            string input2 = "lepel";
+            bool output2 = StringOperations.IsPalindrome(input2);
+
+            Assert.AreEqual(false, output1);
+            Assert.AreEqual(false, output2);
+        }
+    }
+}
---
+++ Dag04.StringDemo.Tests/Dag04.StringDemo/StringOperations.cs
@@ -0,0 +1,23 @@
+﻿namespace Dag04.StringDemo
+{
+    public class StringOperations
+    {
+
+        public static string AToE(string input)
+        {
+            return input.Replace('a', 'e');
+        }
+
+        public static string AToEAndEToI(string input)
+        {
+            return input.Replace('e', 'i').Replace('a', 'e'); 
+        }
+
+        public static bool IsPalindrome(string input)
+        { 
+            string reversedString = input.Reverse().ToString();
+            return input == reversedString;        
+        }
+
+    }
+}
