c8b8e3077435aec58255c9a525099c2d7802602f
Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com <Anthony.Ellenbroek@kcbelastingdiensttrajecten.onmicrosoft.com>
2023-03-29 15:34:28 +0200

    -

--- Day11.BarExcercise.Lib/Day11.BarExcercise.Program/Program.cs
+++ Day11.BarExcercise.Lib/Day11.BarExcercise.Program/Program.cs
@@ -7,43 +7,46 @@ namespace Day11.BarExcercise.Program
         private static void Main(string[] args)
         {
             Order1();
-            Order2();
         }
 
         private static void Order1()
         {
             Bar bar = new Bar();
-            string waiterName = "John";
-            int table = 1;
+            int tableNumber1 = 1;
 
-            bar.TakeOrder(table, waiterName, Drinks.Cola);
-            Console.WriteLine(bar.AskBill(table));
-            bar.PayBill(table, 25m);
-            Console.WriteLine(bar.GetWaiterTipSummary(waiterName));
-        }
-
-        private static void Order2()
-        {
-            Bar bar = new Bar();
             string waiterName1 = "John";
             string waiterName2 = "Barry";
+            string waiterName3 = "Bob";
 
-            int table = 1;
+            List<Drinks> drinks1 = new List<Drinks>();
+            drinks1.Add(Drinks.Cola);
+            drinks1.Add(Drinks.BeerBottle);
+            drinks1.Add(Drinks.BeerBottle);
 
-            bar.TakeOrder(table, waiterName1, Drinks.Cola);
+            List<Drinks> drinks2 = new List<Drinks>();
+            drinks2.Add(Drinks.WineRedBottle);
+            drinks2.Add(Drinks.WineRoseBottle);
 
-            List<Drinks> drinks = new List<Drinks>();
-            drinks.Add(Drinks.Cola);
-            drinks.Add(Drinks.WineRedGlass);
+            List<Drinks> drinks3 = new List<Drinks>();
+            drinks3.Add(Drinks.Cassis);
+            drinks3.Add(Drinks.Sinas);
 
-            bar.TakeOrder(table, waiterName2, drinks);
+            decimal amountPaid1 = 150m;
 
-            bar.TakeOrder(table, waiterName1, Drinks.WineRoseBottle);
+            bar.TakeOrder(tableNumber1, waiterName1, drinks1);
+            bar.TakeOrder(tableNumber1, waiterName2, drinks2);
+            bar.TakeOrder(tableNumber1, waiterName3, drinks3);
+            bar.TakeOrder(tableNumber1, waiterName1, drinks3);
 
-            Console.WriteLine(bar.AskBill(table));
-            bar.PayBill(table, 50m);
+            Console.WriteLine(bar.AskBill(tableNumber1));
+
+            bar.PayBill(tableNumber1, amountPaid1);
+
+            Console.WriteLine();
+            Console.WriteLine($"Amount paid:{amountPaid1}");
             Console.WriteLine(bar.GetWaiterTipSummary(waiterName1));
             Console.WriteLine(bar.GetWaiterTipSummary(waiterName2));
+            Console.WriteLine(bar.GetWaiterTipSummary(waiterName3));
         }
     }
 }
